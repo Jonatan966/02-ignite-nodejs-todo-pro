1284c3adb45e43c00179c4891256b97788d11bf0
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-02 18:10:55 -0300

    feat: create middleware to check user plan

--- src/index.js
+++ src/index.js
@@ -26,7 +26,15 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: "User free plan quota exceeded",
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
